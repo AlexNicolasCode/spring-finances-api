20a986128b912cf98ffb18769876347c4d1d308c
Alex Fernandes <contato.alexnicolas@gmail.com>
2025-11-03 22:52:15 -0300

    feat: set database configuration to run transaction migration

---
+++ src/main/java/com/finances/api/infra/entities/TransactionEntity.java
@@ -0,0 +1,38 @@
+package com.finances.api.infra.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "transactions")
+@SQLRestriction("deleted_at IS NULL")
+@Data
+public class TransactionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Getter
+    private UUID id;
+
+    private String type;
+
+    private float value;
+
+    @Column(name = "from_account_id")
+    private UUID fromAccountId;
+
+    @Column(name = "target_account_id")
+    private UUID targetAccountId;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    @Column(name = "deleted_at", nullable = true)
+    private LocalDateTime deletedAt;
+}
---
+++ src/main/resources/db/migration/V1__create_transactions_table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS transactions (
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      type VARCHAR(255) NOT NULL,
+      value NUMERIC(15,2) NOT NULL,
+      from_account_id UUID NOT NULL,
+      target_account_id UUID NOT NULL,
+      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      deleted_at TIMESTAMP NULL
+);
+
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_updated_at
+    BEFORE UPDATE ON transactions
+    FOR EACH ROW
+    EXECUTE FUNCTION update_updated_at_column();
